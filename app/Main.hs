1504c583fa43bd8287ad17c1bcb6925508735bd1
aryl <endless.sine@gmail.com>
2024-06-19 15:56:50 +0300

    reformatted with ormolu

--- app/Main.hs
+++ app/Main.hs
@@ -1,55 +1,64 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Use if" #-}
 module Main (main) where
 
-import Lib
-import Options.Applicative
-import Data.Time (parseTimeM, defaultTimeLocale, toGregorian)
+import Data.Time (defaultTimeLocale, parseTimeM, toGregorian)
 import Data.Time.Calendar (Day)
-import System.Exit (exitFailure, ExitCode (ExitFailure))
 import Data.Time.Clock
+import Lib
 import Lib (frenchDateToString)
+import Options.Applicative
+import System.Exit (ExitCode (ExitFailure), exitFailure)
 
 data Sample = Sample
-  { date      :: Maybe String
-  , french      :: Bool }
+  { date :: Maybe String,
+    french :: Bool
+  }
 
 sample :: Parser Sample
-sample = Sample
-      <$> optional (strOption
+sample =
+  Sample
+    <$> optional
+      ( strOption
           ( long "date"
-         <> metavar "TARGET"
-         <> help "Target for the greeting" ) )
-      <*> switch
-          ( long "french"
-         <> short 'f'
-         <> help "Whether to be French" )
+              <> metavar "TARGET"
+              <> help "Target for the greeting"
+          )
+      )
+    <*> switch
+      ( long "french"
+          <> short 'f'
+          <> help "Whether to be French"
+      )
 
 main :: IO ()
 main = greet =<< execParser opts
   where
-    opts = info (sample <**> helper)
-      ( fullDesc
-     <> progDesc "Print a greeting for TARGET"
-     <> header "vendemiare - a gregorian to french republican date converter" )
+    opts =
+      info
+        (sample <**> helper)
+        ( fullDesc
+            <> progDesc "Print a greeting for TARGET"
+            <> header "vendemiare - a gregorian to french republican date converter"
+        )
 
 greet :: Sample -> IO ()
 greet (Sample inDate frenchmode) = do
-        today <- case inDate of
-          Just a -> maybe exitFailure return (dateFromArg a)
-          Nothing -> do
-                  toop <- todaysDate
-                  maybe exitFailure return (properDateToFrench (Just toop))
-        case frenchmode of
-          True -> putStrLn $ frenchPrint today
-          False -> putStrLn $ frenchDateToString today
+  today <- case inDate of
+    Just a -> maybe exitFailure return (dateFromArg a)
+    Nothing -> do
+      toop <- todaysDate
+      maybe exitFailure return (properDateToFrench (Just toop))
+  case frenchmode of
+    True -> putStrLn $ frenchPrint today
+    False -> putStrLn $ frenchDateToString today
 
 dateFromArg :: String -> Maybe (Int, RepublicanMonth, Int)
 dateFromArg = properDateToFrench . properDate . dateFromArg1
 
-todaysDate :: IO (Integer,Int,Int) -- :: (year,month,day)
+todaysDate :: IO (Integer, Int, Int) -- :: (year,month,day)
 todaysDate = getCurrentTime >>= return . toGregorian . utctDay
-
 
 properDateToFrench :: Maybe (Integer, Int, Int) -> Maybe (Int, RepublicanMonth, Int)
 properDateToFrench (Just (a, b, c)) = return $ dateToFrench (fromInteger a) b c
@@ -61,4 +70,3 @@ properDate Nothing = Nothing
 
 dateFromArg1 :: String -> Maybe Day
 dateFromArg1 a = parseTimeM False defaultTimeLocale "%Y-%m-%d" a :: Maybe Day
-
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,5 @@
 module Lib
-    (
-    dayToFrench,
+  ( dayToFrench,
     frenchDayToMonth,
     getFrenchMonthFromDay,
     getFrenchMonth,
@@ -9,9 +8,13 @@ module Lib
     dateToFrench,
     dayToFrenchLeap,
     dayOfYear,
-    monthDay, RepublicanMonth(..),
-    frenchPrint, frenchDateToString
-    ) where
+    monthDay,
+    RepublicanMonth (..),
+    frenchPrint,
+    frenchDateToString,
+  )
+where
+
 import Data.List (foldl')
 
 type Year = [Int]
@@ -19,7 +22,7 @@ type Year = [Int]
 -- | nTh number of the Gregorian Year (1 - 366)
 type GregorianDay = Int
 
--- | nTh month of the Republican Year (0-12) 
+-- | nTh month of the Republican Year (0-12)
 type RepublicanMonthNum = Int
 
 type GregorianYear = Int
@@ -30,29 +33,29 @@ type RepublicanDay = Int
 
 type GregorianMonthDay = Int
 
-data RepublicanMonth =  Vendémiaire
-            | Brumaire
-            | Frimaire
-            | Nivôse
-            | Pluviôse
-            | Ventôse
-            | Germinal
-            | Floréal
-            | Prairial
-            | Messidor
-            | Thermidor
-            | Fructidor
-            | Sansculottides
-            deriving (Enum, Ord, Eq, Show)
+data RepublicanMonth
+  = Vendémiaire
+  | Brumaire
+  | Frimaire
+  | Nivôse
+  | Pluviôse
+  | Ventôse
+  | Germinal
+  | Floréal
+  | Prairial
+  | Messidor
+  | Thermidor
+  | Fructidor
+  | Sansculottides
+  deriving (Enum, Ord, Eq, Show)
 
 -- I/O
 
 frenchPrint :: (Int, RepublicanMonth, Int) -> String
 frenchPrint (year, month, day) = "\ESC[0;34m" ++ show year ++ " " ++ "\ESC[0;0m" ++ show month ++ " " ++ "\ESC[0;31m" ++ show day ++ "\ESC[0;0m"
 
-frenchDateToString :: (Int, RepublicanMonth, Int) -> String 
-frenchDateToString (year, month, day) = show year ++ " " ++ show month ++ " " ++ show day 
-
+frenchDateToString :: (Int, RepublicanMonth, Int) -> String
+frenchDateToString (year, month, day) = show year ++ " " ++ show month ++ " " ++ show day
 
 -- Date Handling:
 
@@ -67,30 +70,32 @@ dayToFrenchLeap a = mod (a + 100) 366 + 1
 
 dateToFrench :: GregorianYear -> GregorianMonth -> GregorianMonthDay -> (Int, RepublicanMonth, Int)
 dateToFrench year month day = (yearToFrench year day', getFrenchMonthFromDay frenchDay, monthDay frenchDay)
-        where frenchDay = dayToFrench_ year day'
-              day' = dayOfYear year month day
+  where
+    frenchDay = dayToFrench_ year day'
+    day' = dayOfYear year month day
 
 leapyear :: Year
-leapyear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+leapyear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 normalyear :: Year
-normalyear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
-
+normalyear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 dayOfYear :: Int -> Int -> Int -> Int
-dayOfYear year month day | isLeap year = foldl' (+) 0 (take (month - 1) leapyear) + day
+dayOfYear year month day
+  | isLeap year = foldl' (+) 0 (take (month - 1) leapyear) + day
   | otherwise = foldl' (+) 0 (take (month - 1) normalyear) + day
 
-
 dayToFrench_ :: Int -> Int -> Int
-dayToFrench_ year day | isLeap year = dayToFrenchLeap day
-                         | otherwise = dayToFrench day
+dayToFrench_ year day
+  | isLeap year = dayToFrenchLeap day
+  | otherwise = dayToFrench day
 
 frenchDayToMonth :: Int -> Int
 frenchDayToMonth a = floor ((fromIntegral a / 30) :: Double)
 
 yearToFrench :: Int -> Int -> Int
-yearToFrench year civil_day | isLeap year && civil_day < 266 = year - 1792
+yearToFrench year civil_day
+  | isLeap year && civil_day < 266 = year - 1792
   | civil_day < 266 && not (isLeap year) = year - 1792
   | otherwise = year - 1791
 
@@ -98,9 +103,9 @@ isLeap :: Int -> Bool
 isLeap a = (a `mod` 4 == 0) && (a `mod` 100 /= 0) || (a `mod` 400 == 0)
 
 getFrenchMonthFromDay :: RepublicanDay -> RepublicanMonth
-getFrenchMonthFromDay a | a > 360 = Sansculottides
-                        | otherwise  = toEnum $ frenchDayToMonth a
+getFrenchMonthFromDay a
+  | a > 360 = Sansculottides
+  | otherwise = toEnum $ frenchDayToMonth a
 
 getFrenchMonth :: RepublicanMonthNum -> RepublicanMonth
 getFrenchMonth = toEnum
-
